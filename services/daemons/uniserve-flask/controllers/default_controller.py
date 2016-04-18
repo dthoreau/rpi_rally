cfaedace5f5d1799b7b69919bef2802255546bfa
Dominic Thoreau <dominic_git@thoreau.kiwi>
2016-04-18 20:30:56 +0000

    Stubs for in-progress flask server

---
+++ services/daemons/uniserve-flask/controllers/default_controller.py
@@ -0,0 +1,15 @@
+
+def ddot_put(red = None, green = None, blue = None, x = None, y = None) -> str:
+    return 'do some magic!'
+
+def dot_put(stdin) -> str:
+    return 'do some magic!'
+
+def monoframe_put() -> str:
+    return 'do some magic!'
+
+def monoline_put() -> str:
+    return 'do some magic!'
+
+def show_get() -> str:
+    return 'do some magic!'
