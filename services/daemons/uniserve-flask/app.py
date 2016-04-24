3595fdb219200667642131f98b95c04e6fe45ce8
Dominic Thoreau <dominic_git@thoreau.kiwi>
2016-04-24 20:32:46 +0000

    Controller development
    Although it still isn't working :(

--- services/daemons/uniserve-flask/app.py
+++ services/daemons/uniserve-flask/app.py
@@ -5,4 +5,4 @@ import connexion
 if __name__ == '__main__':
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.add_api('swagger.yaml', arguments={'title': 'A second cut at writing the code initial formed in web.py for the PiMoroni UnicornHAT\n'})
-    app.run(port=8080)
+    app.run(port=8080,debug=True)
--- services/daemons/uniserve-flask/controllers/default_controller.py
+++ services/daemons/uniserve-flask/controllers/default_controller.py
@@ -1,15 +1,38 @@
+from flask import jsonify
+import unicornhat as UH
 
 def all_pixels_get() -> str:
     return 'do some magic!'
 
 def dot_x_put(colour, x, y) -> str:
-    return 'do some magic!'
+    paint_dot(x, y, colour.red, colour.green, colour.blue)
 
 def monoframe_pixelmap_put(pixelmap, colour) -> str:
     return 'do some magic!'
 
 def monoline_put(colour, line, pixelmap) -> str:
-    return 'do some magic!'
+    paint_monoline(line, pixelmap, colour.red, colour.green, colour.blue)
+    return 200
+    return({},undef, 200)
 
 def show_get() -> str:
-    return 'do some magic!'
+    paint_monoline(2, 'CC', 40,40,40)
+    UH.show()
+    return 'Make it happen, captain!'
+
+def paint_dot(x,y,red,green,blue):
+    UH.set_pixel(x,y,red,green,blue)
+
+def paint_monoline(line, details, red, green, blue):
+    index = [7,6,5,4,3,2,1,0]
+    mappings = int(details, 16)
+    for x in range(8):
+        testx = 2**index[x]
+        if mappings >= testx:
+             mappings  = mappings - testx
+             paint_dot(x, line, red,green,blue)
+        else:
+             paint_dot(x, line, 0,0,0)
+
+def sp_string(line, n):
+        return [line[i:i+n] for i in range(0, len(line), n)]
